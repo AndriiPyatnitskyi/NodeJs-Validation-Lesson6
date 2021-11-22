9ff6514328276c3c10a02e1b72b737906c55621f
AndriiPyatnitskyi <pyatnitskiy.a@gmail.com>
2021-11-22 17:44:50 +0200

    add validation fields

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -1,6 +1,9 @@
+import {Role} from "../dto/account";
+
 const accountSchema = {
     body: {
-        name: { type: 'string', min: 3, max: 15 }
+        name: { type: 'string', min: 3, max: 15 },
+        role: {type: 'string', enum: [ Role.USER.toString(), Role.ADMIN.toString(), Role.ANONYMOUS.toString() ]}
     },
 };
 
